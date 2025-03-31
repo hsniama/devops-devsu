3cdb4e588e67791d5642f6239802cc7e3164ea01
Henry Niama <henrysebastian.niama@alumno.ucjc.edu>
2025-03-30 22:52:20 -0500

    Creacion de manifiestos k8s

--- demo/urls.py
+++ demo/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
+
+# urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', include('api.urls')),  # Notar que ahora es '' en vez de 'api/'
+# ]
+
